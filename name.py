4706c87ec3646feec8a1bc5bf5a90eae6f6d94a5
Guest User <Guest@idefi-iifr-spc-upd-18.local>
2018-05-04 12:38:25 +0200

    file

---
+++ name.py
@@ -0,0 +1,10 @@
+print("tell me your name")
+yourName=input()
+print("and what is your age?")
+yourAge=int(input())
+
+
+if yourAge > 25:
+	print("Your name is " + yourName + " and you are old")
+else:
+	print("you are young")
